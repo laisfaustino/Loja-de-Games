f12be590914424f254c2afdcab9c454d8a841575
Lais Faustino <laisfaustino@hotmail.com>
2023-04-20 09:50:11 -0300

    Update ProdutoRepository.java

--- src/main/java/com/generation/lojagames/repository/ProdutoRepository.java
+++ src/main/java/com/generation/lojagames/repository/ProdutoRepository.java
@@ -1,7 +1,7 @@
  package com.generation.lojagames.repository;
 
+import java.math.BigDecimal;
 import java.util.List;
-
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
@@ -11,4 +11,5 @@ import com.generation.lojagames.model.Produto;
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 
 	List<Produto> findAllByNomeContainingIgnoreCase(@Param("nome") String nome);
+	List<Produto> findAllByPrecoContainingIgnoreCase(@Param("preco") BigDecimal preco);
 }
